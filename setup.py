3ef16ffd8f6debe68c156d7c61892823d5702078
maksyuki <maksyuki@126.com>
2025-12-21 23:12:30 +0800

    chore: mv kianV from username1 to username2

--- setup.py
+++ setup.py
@@ -7,3 +7,6 @@ MPW_PATH = f'{ROOT_PATH}/rtl/mini/mpw'
 os.chdir(MPW_PATH)
 os.system('rm -rf .build')
 os.system('mkdir .build')
+
+
+os.system('git clone --recursive https://github.com/Wren6991/Hazard3.git core/username1/')
