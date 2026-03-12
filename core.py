7c93d41fc57c1767a72f5c7ab04e27cc2be12be5
maksyuki <maksyuki@126.com>
2026-03-12 12:41:46 +0800

    chore: update time peroid in file header

--- core.py
+++ core.py
@@ -20,20 +20,6 @@ simu = sys.argv[1]
 print(f'simu: {simu}')
 
 
-lines = ''
-# remove parallel case statement
-with open(f'{ROOT_PATH}/rtl/mini/core/picorv32.v', 'r', encoding='utf-8') as fp:
-    for v in fp:
-        if '// synopsys parallel_case' in v:
-            lines += v.replace('// synopsys parallel_case', '')
-        elif '// synopsys full_case parallel_case' in v:
-            lines += v.replace('// synopsys full_case parallel_case', '')
-        else:
-            lines += v
-
-with open(f'{ROOT_PATH}/rtl/mini/core/picorv32_ver.v', 'w', encoding='utf-8') as fp:
-    fp.writelines(lines)
-
 os.chdir(MPW_PATH)
 os.system('rm -rf ./.build/core')
 os.system('cp -rf ./core ./.build/')
